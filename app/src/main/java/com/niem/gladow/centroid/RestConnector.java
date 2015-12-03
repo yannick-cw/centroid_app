6b7884bd5f0b9710c72bf88fe84af7c1ed3baee6
yannick-cw <1202roller>
2015-12-03 18:05:27 +0100

    added raspy as server!

--- app/src/main/java/com/niem/gladow/centroid/RestConnector.java
+++ app/src/main/java/com/niem/gladow/centroid/RestConnector.java
@@ -17,7 +17,7 @@ import java.net.URL;
  */
 public class RestConnector extends AsyncTask<String, String, String> {
     private static final String POST = "1", GET = "2", SEND = "3";
-    private static final String HOST_ADDRESS = "http://10.181.26.131:8080/java_rest";
+    private static final String HOST_ADDRESS = "http://schnutentier.ddns.net/java_rest";
     private Context context;
     private static boolean postSuccess = false;
     private static boolean getSuccess = false;
