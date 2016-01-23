9ef943271342e14ec4753959fda15de693142228
yannick <yangla@gmx.de>
2016-01-23 17:05:59 +0100

    fixes crashes

--- app/src/main/java/com/niem/gladow/centroid/GpsDataHandler.java
+++ app/src/main/java/com/niem/gladow/centroid/GpsDataHandler.java
@@ -8,18 +8,22 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 /**
  * The GpsDataHandler is the central point to manage the phones GPS data
  */
-public class GpsDataHandler implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class GpsDataHandler implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
     private GoogleApiClient googleApiClient;
     private Context context;
     private Location lastLocation;
     private static final String SEND_GPS = "/android/currentGPS/";
     private static String OWN_NUMBER;
+    protected LocationRequest mLocationRequest;
+    private boolean locationUpdateStarted = false;
+
 
     //the gps data is send on creation of the class
     public GpsDataHandler (Context context) {
@@ -35,6 +39,7 @@ public class GpsDataHandler implements GoogleApiClient.ConnectionCallbacks, Goog
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+        createLocationRequest();
     }
 
     @Override
@@ -51,15 +56,35 @@ public class GpsDataHandler implements GoogleApiClient.ConnectionCallbacks, Goog
                                                               + lastLocation.getLongitude()+"/"
                                                               + lastLocation.getLatitude());
             Log.d("lastLocation", String.valueOf(lastLocation.getLongitude()));
+
+            if(!locationUpdateStarted) {
+                startLocationUpdates();
+                locationUpdateStarted = true;
+            }
         }
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        lastLocation = location;
+    }
 
     @Override
     public void onConnectionFailed(ConnectionResult result) {
         Log.i("GpsDataHandler", "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
     }
 
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(100000);
+        mLocationRequest.setFastestInterval(50000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this);
+    }
 
     @Override
     public void onConnectionSuspended(int cause) {
